f4698107d834f2cea952dbac3ec2b6e168091e87
kienz7777 <kienvu7777@gmail.com>
2021-03-18 22:26:44 +0700

    merge one recursion

--- algorithm/mergeOne.js
+++ algorithm/mergeOne.js
@@ -1,1 +1,22 @@
 // flatten([[[1, [1.1]], 2, 3], [4, 5]]); // [1, 1.1, 2, 3, 4, 5]
+
+const k = [[[1, [1.1]], 2, 3], [4, 5]];
+
+const getNestedArr = (arr) => {
+    let A = [];
+
+    arr.forEach(item => {
+        if(Array.isArray(item)){
+            //A.push(getNestedArr(item))
+            A = A.concat(getNestedArr(item))
+        }
+        else{
+            //A.push(item);
+            A = A.concat(item)
+        }
+    });
+
+    return A;
+}
+
+console.log(getNestedArr(k));
